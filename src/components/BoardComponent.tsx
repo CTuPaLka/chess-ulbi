e66f130f05c496894d1f33a798a66cf7d378d28b
chicory <grifon.rudenko@yandex.com>
2023-08-08 22:21:28 +0300

    commit 3

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Board } from "../models/Board";
 import CellComponent from "./CellComponent";
 import { Cell } from "../models/Cell";
@@ -11,14 +11,34 @@ interface BoardProps {
 const BoardComponent: React.FC<BoardProps> = ({ board, setBoard }) => {
 	const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
 	
+	function clickOnCell(cell: Cell){
+		cell.figure && setSelectedCell(cell)
+	}
+
+	useEffect(()=>{
+		highLightCells()
+	}, [selectedCell])
+
+	function highLightCells(){
+		board.highLightCells(selectedCell)
+		updateBoard()
+	}
+
+	function updateBoard(){
+		const newBoard = board.getCopyBoard()
+		setBoard(newBoard)
+	}
+
 	return (
 		<div className="board">
 			{board.cells.map((row, index) =>
 				<React.Fragment key={index}>
 					{row.map(cell =>
 						<CellComponent 
+						clickOnCell={clickOnCell}
 						cell={cell}
 						key={cell.id}
+						selected={cell.x === selectedCell?.x && cell.y===selectedCell?.y}
 						/>
 					)}
 				</React.Fragment>
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -4,11 +4,20 @@ import { Cell } from "../models/Cell";
 interface CellProps{
 	cell: Cell
 	selected: boolean;
+	clickOnCell: (cell: Cell)=>void
 }
 
-const CellComponent: React.FC<CellProps>=({cell, selected})=> {
-  return <div className={['cell', cell.color, selected ? "selected" : ""].join(' ')}>
-	{cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
+const CellComponent: React.FC<CellProps>=({cell, selected, clickOnCell})=> {
+  return <div 
+  className={['cell', cell.color, selected ? "selected" : ""].join(' ')}
+  onClick={()=>clickOnCell(cell)}
+  style={{background: cell.available && cell.figure ? 'green' : ''}}
+  >
+
+	{cell.available && !cell.figure && <div className={'avaible'}/>}
+	{cell.figure?.logo && <img src={cell.figure.logo} alt=""
+	
+	/>}
   </div>;
 }
 
